80a8a378a7796a6aa51bdc9a06ec78beb7023638
sachin <sachin.aralapura@gmail.com>
2025-10-20 17:30:30 +0530

    heaps

--- 10-heaps/heap.h
+++ 10-heaps/heap.h
@@ -1,4 +1,4 @@
-
+#include <algorithm>
 #include <vector>
 using namespace std;
 template <class T, class Compare = less<T>> class BinaryHeap {
@@ -11,12 +11,12 @@ template <class T, class Compare = less<T>> class BinaryHeap {
     int parentNodeIndex(int);
     void heapifyUp(int);
     void heapifyDown(int);
-    inline bool isLess(const T &a, const T &b) { return _compare(a, b); }
+    inline bool compare(const T &a, const T &b) { return _compare(a, b); }
 
   public:
     BinaryHeap(const Compare &comp = Compare());
-
-    const T getNode(int) const;
+    BinaryHeap(const vector<T> arr);
+    T getNode(int) const;
     void buildHeapify(vector<T>);
     T leftChild(int);
     T rightChild(int);
@@ -24,9 +24,12 @@ template <class T, class Compare = less<T>> class BinaryHeap {
     void insertNode(T);
     T removeRootNode();
     void swap(int, int);
+    bool isEmpty() { return _arr.empty(); }
 };
 
 template <class T, class Compare> BinaryHeap<T, Compare>::BinaryHeap(const Compare &comp) : _compare(comp) {}
+
+template <class T, class Compare> BinaryHeap<T, Compare>::BinaryHeap(const vector<T> arr) { this->buildHeapify(arr); }
 
 template <class T, class Compare> void BinaryHeap<T, Compare>::swap(int _x, int _y) {
     int n = _arr.size();
@@ -35,6 +38,7 @@ template <class T, class Compare> void BinaryHeap<T, Compare>::swap(int _x, int 
     _arr[_x] = _arr[_y];
     _arr[_y] = temp;
 }
+
 template <class T, class Compare> int BinaryHeap<T, Compare>::leftChildIndex(int _index) {
     int n = _arr.size();
     int left_index = 2 * _index + 1;
@@ -55,20 +59,23 @@ template <class T, class Compare> int BinaryHeap<T, Compare>::parentNodeIndex(in
     return (_index - 1) / 2;
 }
 
-template <class T, class Compare> const T BinaryHeap<T, Compare>::getNode(int _index) const { return _arr[_index]; }
+template <class T, class Compare> T BinaryHeap<T, Compare>::getNode(int _index) const { return _arr[_index]; }
 
 template <class T, class Compare> T BinaryHeap<T, Compare>::leftChild(int _index) {
     int index = this->leftChildIndex(_index);
+    if (index == -1) throw "No left child";
     return _arr[index];
 }
 
 template <class T, class Compare> T BinaryHeap<T, Compare>::rightChild(int _index) {
     int index = this->rightChildIndex(_index);
+    if (index == -1) throw "No right child";
     return _arr[index];
 }
 
 template <class T, class Compare> T BinaryHeap<T, Compare>::parentNode(int _index) {
     int index = this->parentNodeIndex(_index);
+    if (index == -1) throw "No parent node";
     return _arr[index];
 }
 
@@ -79,14 +86,14 @@ template <class T, class Compare> void BinaryHeap<T, Compare>::insertNode(T _dat
 
 template <class T, class Compare> void BinaryHeap<T, Compare>::heapifyUp(int _index) {
     int curr = _index;
-    while (curr != 0 && isLess(parentNode(curr), _arr[curr])) {
-        swap(parentNode(curr), getNode(curr));
+    while (curr != 0 && compare(parentNode(curr), _arr[curr])) {
+        swap(parentNodeIndex(curr), curr);
         curr = parentNodeIndex(curr);
     }
 }
 
 template <class T, class Compare> T BinaryHeap<T, Compare>::removeRootNode() {
-    if (_arr.empty()) throw "Error";
+    if (_arr.empty()) throw "Empty Heap";
     T root = _arr[0];
     _arr[0] = _arr[_arr.size() - 1];
     _arr.pop_back();
@@ -96,18 +103,33 @@ template <class T, class Compare> T BinaryHeap<T, Compare>::removeRootNode() {
 
 template <class T, class Compare> void BinaryHeap<T, Compare>::heapifyDown(int _index) {
     int n = _arr.size();
-    int largest = _index;
-    while (true) {
-        int left = leftChildIndex(_index);
-        int right = rightChildIndex(_index);
+    int curr = _index;
 
-        if (left < n && isLess(leftChild(_index), getNode(largest))) largest = left;
-        if (right < n && isLess(rightChild(_index), getNode(largest))) largest = right;
-        if (largest != _index) {
-            swap(getNode(largest), getNode(_index));
-            _index = largest;
-        } else
+    while (true) {
+        int left = leftChildIndex(curr);
+        int right = rightChildIndex(curr);
+        int highest_priority = curr; // Assume current node is highest priority
+
+        // Compare with Left Child
+        // Check if left child exists AND left child has higher priority than current 'highest'
+        if (left != -1 && compare(_arr[highest_priority], _arr[left])) {
+            highest_priority = left;
+        }
+
+        // Compare with Right Child
+        // Check if right child exists AND right child has higher priority than current 'highest'
+        if (right != -1 && compare(_arr[highest_priority], _arr[right])) {
+            highest_priority = right;
+        }
+
+        // If a child had higher priority, swap and continue down
+        if (highest_priority != curr) {
+            std::swap(_arr[curr], _arr[highest_priority]);
+            curr = highest_priority; // Continue down from the swapped child's position
+        } else {
+            // Heap property satisfied at this node
             break;
+        }
     }
 }
 
